b0e894be787ec4931f13f4ed0d6a9957e56d0f65
O-Jun-S <59322318+O-Jun-S@users.noreply.github.com>
2020-01-09 09:59:29 +0900

    Add more info.
    Add more info of power, fibonacci.

--- test.py
+++ test.py
@@ -8,3 +8,8 @@ print(num2.sqrt(), num2.log(2), num2.binary_number(), num2.even_odd())
 
 num3 = N.Number(15)
 print(num3.Fizz_Buzz())
+
+num4 = N.Number(4)
+print(num4.fib_list(), num4.fib())
+num4.power(4)
+print(num4.num)
